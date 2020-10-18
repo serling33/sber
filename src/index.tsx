e255a6451dacbdc11c7ed493c93adf13b0a53923
Александр Артемьев <sve567467@yandex.ru>
2020-10-18 10:00:25 +0300

    fix: errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import emotionNormalize from 'emotion-normalize'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import * as serviceWorker from './serviceWorker'
 import { css, Global, jsx } from '@emotion/core'
 import Auth from './pages/Auth'
 import { Provider } from 'react-redux'
@@ -240,8 +239,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
